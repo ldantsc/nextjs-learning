115f31a47820cdc04fd66eaf23119351663409b6
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-11-17 20:14:05 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400',
+    display: 'swap'
+});
